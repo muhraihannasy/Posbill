21d3b5c73c2fbe2b98eced137e1ba2a382700a45
muhraihannasy <muhraihannasy@gmail.com>
2022-08-12 15:46:18 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./assets/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#C1136E",
+        secondary: "#FFD140",
+        third: "#922775",
+        fourty: "rgba(106, 0, 56, 58%)",
+      },
+      screens: {
+        smPhone: '387px',
+        mdPhone: '490px',
+        tablet: '857px',
+        mdTablet: '1005px',
+        lgTablet: '1148px',
+      },
+    },
+  },
+  plugins: [],
+}
