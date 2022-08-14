3c87ba7a21efa9b59a33ba83cfb9bfa962355ada
muhraihannasy <muhraihannasy@gmail.com>
2022-08-14 10:02:58 +0700

    fourty commit

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,20 +1,18 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:60,
+    nav:true,
+    navText: ["<img src='http://127.0.0.1:5500/assets/images/home/icn-l.png' class='prev owl-prev'> ","<img src='http://127.0.0.1:5500/assets/images/home/icn-r.png' class='next owl-next'>"],
     responsiveClass:true,
     responsive:{
         0:{
             items:1,
-            nav:true
         },
         600:{
             items:2,
-            nav:false
         },
         1000:{
             items:4,
-            nav:true,
-            loop:false
         }
     }
 })
